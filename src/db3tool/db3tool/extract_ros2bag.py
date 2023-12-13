9a51bf7985849b8e246d04ddfb3074fff0130846
u000000 <adasko99a@gmail.com>
2023-12-13 23:43:52 +0100

    final

--- src/db3tool/db3tool/extract_ros2bag.py
+++ src/db3tool/db3tool/extract_ros2bag.py
@@ -9,9 +9,15 @@ import numpy as np
 class ExtractImagesNode(Node):
     def __init__(self):
         super().__init__('extract_ros2bag')
-        self.counter = 0
-                
+        self.counter = 0 
         self.bridge = cv_bridge.CvBridge()
+        
+        # Parameters
+        self.data_folder = self.declare_parameter("data_folder_path","").value
+        
+        if self.data_folder == "":
+            self.get_logger().error("Need folder path --ros-args --param data_folder_path:=<folder-path>")
+            exit()
         
         self.image_raw_subscriber_ = self.create_subscription(
             Image, 
@@ -23,10 +29,10 @@ class ExtractImagesNode(Node):
         
     def image_callback(self, msg: Image):
         try:
-            cv_img = self.bridge.imgmsg_to_cv2(msg, 'mono16')
-            cv2.imwrite('num_' + str(self.counter) + '.png', cv_img)
+            cv_img = self.bridge.imgmsg_to_cv2(msg, 'mono8')
+            result = cv2.imwrite(self.data_folder + '/num_' + str(self.counter) + '.png', cv_img)
             self.counter += 1
-            self.get_logger().info('Image saved: ' + 'num_' + str(self.counter) + '.png')
+            self.get_logger().info(str(self.counter) + ' saved? ' + str(result))
         except Exception as e:
             self.get_logger().error('Error processing image: %s' % str(e))
         
